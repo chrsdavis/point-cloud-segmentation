740e15244c9ada7eee145c975d27971632ba5ade
chrsdavis <84039785+chrsdavis@users.noreply.github.com>
2024-04-28 18:30:45 -0400

    train/val/test splits

--- format_data.py
+++ format_data.py
@@ -1,4 +1,5 @@
 import os
+import numpy as np
 
 root = 'Rellis-3D'
 pcl_dir = 'os1_cloud_node_kitti_bin'
@@ -35,3 +36,37 @@ print('Writing to config/data/pcl.lst')
 with open('config/data/pcl.lst', 'w') as file:
     for f in pcl_files:
         file.write(f + '\n')
+
+n = len(pcl_files)
+
+indices = np.arange(n)
+np.random.shuffle(indices)
+
+train_ratio = 0.6
+validation_ratio = 0.2
+test_ratio = 0.2
+
+train_end = int(train_ratio * n)
+validation_end = train_end + int(validation_ratio * n)
+
+train_indices = indices[:train_end]
+val_indices = indices[train_end:validation_end]
+test_indices = indices[validation_end:]
+
+rel_pcl_files = [os.path.relpath(f, root) for f in pcl_files]
+rel_lab_files = [os.path.relpath(f, root) for f in lab_files]
+
+print('Writing to config/train.lst')
+with open('config/train.lst', 'w') as file:
+    for i in train_indices:
+        file.write(f'{rel_pcl_files[i]}\t{rel_lab_files[i]}\n')
+
+print('Writing to config/val.lst')
+with open('config/val.lst', 'w') as file:
+    for i in val_indices:
+        file.write(f'{rel_pcl_files[i]}\t{rel_lab_files[i]}\n')
+
+print('Writing to config/test.lst')
+with open('config/test.lst', 'w') as file:
+    for i in test_indices:
+        file.write(f'{rel_pcl_files[i]}\t{rel_lab_files[i]}\n')
